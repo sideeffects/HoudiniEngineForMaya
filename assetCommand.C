ed2a7d53b6a33a04a80834aef1900292de726f40
Damian Campeanu <damian@sidefx.com>
2013-01-21 13:06:58 -0500

    Warning fix and vc11 proj update.

--- assetCommand.C
+++ assetCommand.C
@@ -31,7 +31,7 @@ MStatus AssetCommand::doIt( const MArgList& args )
 {
 
     MStatus status;
-    for ( int ii = 0; ii < args.length(); ii++ )
+    for ( unsigned int ii = 0; ii < args.length(); ii++ )
     {
 	// -load load an otl file
 	// expected arguments: otl_file_name - the name of the otl file to load
@@ -48,7 +48,7 @@ MStatus AssetCommand::doIt( const MArgList& args )
             && MS::kSuccess == status )
         {
 	    myOperationType = kOperationSaveHip;
-	    myHIPFilePath = args.asString( ++ii );	    
+	    myHIPFilePath = args.asString( ++ii );
 	}
         else
         {

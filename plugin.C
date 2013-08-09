5a71cf149766e1a555c0a5f33f6ef0788e400a92
Andrew Wong <awong@sidefx.com>
2013-08-09 16:56:19 -0400

    Add some error checking when initializing HAPI

--- plugin.C
+++ plugin.C
@@ -1,3 +1,5 @@
+#include <maya/MGlobal.h>
+
 #include <maya/MFnPlugin.h>
 
 #include "AssetCommand.h"
@@ -9,6 +11,11 @@
 bool
 initializeHAPI()
 {
+    if(HAPI_IsInitialized() == HAPI_RESULT_SUCCESS)
+    {
+	return true;
+    }
+
     HAPI_Result hstat = HAPI_RESULT_SUCCESS;
 
     MString otl_dir(getenv("HAPI_OTL_PATH"));
@@ -17,13 +24,16 @@ initializeHAPI()
     MString hfs(getenv("HAPI_PATH"));
     if (hfs == "")
     {
-	cerr << "*Error*: HAPI_PATH not found" << endl;
-	throw HAPIError("HFS directory not found");
+	MGlobal::displayError("HAPI_PATH enviornment variable is not set.");
+	return false;
     }
-    hfs += "/";
-    cerr << "hfs: " << hfs.asChar() << endl;
+
     hstat = HAPI_Initialize(hfs.asChar(), otl_dir.asChar(),
 	    dso_dir.asChar(), true, -1);
+    if(hstat != HAPI_RESULT_SUCCESS)
+    {
+	return false;
+    }
 
     return true;
 }
@@ -34,8 +44,13 @@ initializePlugin(MObject obj)
     MStatus status;
     MFnPlugin plugin(obj, "Side Effects", "1.0", "Any");
     
-    if(!initializeHAPI())
+    if(initializeHAPI())
     {
+	MGlobal::displayInfo("Houdini Engine initialized successfully.");
+    }
+    else
+    {
+	MGlobal::displayInfo("Houdini Engine failed to initialize.");
 	return MStatus::kFailure;
     }
 

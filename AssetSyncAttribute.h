e4d935b98636010756c7ca6df514d2c40884bbe6
Andrew Wong <awong@sidefx.com>
2013-09-30 12:33:31 -0400

    Move code for adding parameter attributes from Asset to AssetSyncAttribute

--- AssetSyncAttribute.h
+++ AssetSyncAttribute.h
@@ -21,6 +21,16 @@ class AssetSyncAttribute : public AssetSync
 	virtual MStatus redoIt();
 
     protected:
+	void addAttrTo(MObject& child, MObject* parent);
+
+        MObject createAttr(HAPI_ParmInfo& parm);
+        MObject createStringAttr(HAPI_ParmInfo& parm, MString& longName, MString& shortName, MString& niceName);
+        MObject createNumericAttr(HAPI_ParmInfo& parm, MString& longName, MString& shortName, MString& niceName);
+
+        void buildParms();
+        int buildAttrTree(HAPI_ParmInfo* myParmInfos, MObject* parent, int current, int start);
+
+    protected:
 	const MObject myAssetNodeObj;
 
 	HAPI_NodeInfo myNodeInfo;

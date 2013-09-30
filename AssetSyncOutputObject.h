04e81290ada153de66886bfeaea80a483235fcca
Adrian Saldanha <asaldanha@sidefx.com>
2013-09-30 14:37:36 -0400

    Two forgotten files with r188735.

---
+++ AssetSyncOutputObject.h
@@ -0,0 +1,39 @@
+#ifndef _AssetSyncOutputObject_h__
+#define _AssetSyncOutputObject_h__
+
+#include <vector>
+
+#include <maya/MDagModifier.h>
+#include <maya/MPlug.h>
+
+#include "AssetSync.h"
+
+class AssetSyncOutputObject : public AssetSync
+{
+    public:
+	AssetSyncOutputObject(
+		const MPlug &outputPlug,
+		const MObject &assetNodeObj
+		);
+	virtual ~AssetSyncOutputObject();
+
+	virtual MStatus doIt();
+	virtual MStatus undoIt();
+	virtual MStatus redoIt();
+
+    protected:
+	MStatus createOutputObject();
+
+	const MPlug myOutputPlug;
+	const MObject myAssetNodeObj;
+
+	MDagModifier myDagModifier;
+
+	// TODO: change this into an AssetSyncOutputGeoPart
+	typedef std::vector<AssetSync*> AssetSyncs;
+	AssetSyncs myAssetSyncs;
+};
+
+
+
+#endif

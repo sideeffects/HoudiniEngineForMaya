3c68c9074983fa152b67c7239d4b2feca15f7306
Adrian Saldanha <asaldanha@sidefx.com>
2013-09-30 14:37:36 -0400

    Two forgotten files with r188735.

---
+++ AssetSyncOutputObject.C
@@ -0,0 +1,81 @@
+#include "AssetSyncOutputObject.h"
+
+#include <maya/MGlobal.h>
+#include <maya/MPlug.h>
+
+#include <maya/MFnDependencyNode.h>
+#include <maya/MFnDagNode.h>
+
+#include "AssetNode.h"
+#include "AssetSyncOutputGeoPart.h"
+#include "AssetSyncOutputInstance.h"
+
+AssetSyncOutputObject::AssetSyncOutputObject(
+	const MPlug &outputPlug,
+	const MObject &assetNodeObj )
+    : myOutputPlug(outputPlug),
+      myAssetNodeObj(assetNodeObj)
+{}
+
+AssetSyncOutputObject::~AssetSyncOutputObject()
+{
+    for(AssetSyncs::const_iterator it = myAssetSyncs.begin();
+	    it != myAssetSyncs.end();
+	    it++)
+    {
+	delete *it;
+    }
+    myAssetSyncs.clear();
+}
+
+MStatus
+AssetSyncOutputObject::doIt()
+{
+    MStatus status;
+    // Create our parts.
+    // An object just contains a number of parts, and no
+    // other information.
+    MFnDependencyNode assetNodeFn(myAssetNodeObj, &status);
+
+    // Parts
+    MPlug partsPlug = myOutputPlug.child(AssetNode::outputParts);
+    int partCount = partsPlug.evaluateNumElements(&status);
+    CHECK_MSTATUS_AND_RETURN_IT(status);
+    for (int i=0; i<partCount; i++)
+    {
+	AssetSync* sync = new AssetSyncOutputGeoPart(partsPlug[i], myAssetNodeObj);
+	cerr << "Creating an output part." << endl;
+	sync->doIt();
+	myAssetSyncs.push_back(sync);
+    }
+
+    return MStatus::kSuccess;
+}
+
+MStatus
+AssetSyncOutputObject::undoIt()
+{
+    for(AssetSyncs::reverse_iterator iter = myAssetSyncs.rbegin();
+	    iter != myAssetSyncs.rend();
+	    iter++)
+    {
+	(*iter)->undoIt();
+    }
+    myDagModifier.undoIt();
+
+    return MStatus::kSuccess;
+}
+
+MStatus
+AssetSyncOutputObject::redoIt()
+{
+    myDagModifier.doIt();
+    for(AssetSyncs::iterator iter = myAssetSyncs.begin();
+	    iter != myAssetSyncs.end();
+	    iter++)
+    {
+	(*iter)->redoIt();
+    }
+
+    return MStatus::kSuccess;
+}

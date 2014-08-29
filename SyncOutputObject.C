dd3f9516f6a96e376f6cf09cda1ea99f6eb23b60
Andrew Wong <awong@sidefx.com>
2014-08-29 19:27:36 -0400

    Revert "HAPI: Maya: Simply playFromCache attribute instead of making a connection"
    The fluidShape.playFromCache actually needs to be a connection, because
    the attribute is not saved to the scene file.

--- SyncOutputObject.C
+++ SyncOutputObject.C
@@ -249,13 +249,6 @@ SyncOutputObject::createFluidShape()
         MFnDagNode partFluidTransformFn(transform, &status);
         CHECK_MSTATUS_AND_RETURN_IT(status);
 
-        // set playFromCache
-        status = myDagModifier.newPlugValueBool(
-                partVolumeFn.findPlug("playFromCache"),
-                true
-                );
-        CHECK_MSTATUS_AND_RETURN_IT(status);
-
         MObject velConverter;
 
         bool doneDensity = false;
@@ -392,6 +385,11 @@ SyncOutputObject::createFluidShape()
                 status = myDagModifier.connect(srcPlug, dstPlug);
                 CHECK_MSTATUS_AND_RETURN_IT(status);
             }
+
+            srcPlug = myOutputPlug.child(AssetNode::outputObjectFluidFromAsset);
+            dstPlug = partVolumeFn.findPlug("playFromCache");
+            status = myDagModifier.connect(srcPlug, dstPlug);
+            CHECK_MSTATUS_AND_RETURN_IT(status);
 
             // inOffset must be initialized or bugs will be had; and the math is
             // simpler with dimensions all equal to 1.

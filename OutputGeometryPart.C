8df2012b98239dcfac323e1443e973a881d1da3f
Andrew Wong <awong@sidefx.com>
2015-07-02 14:43:50 -0400

    Fixed a crash when there are groups on output that contains both primitives and points

--- OutputGeometryPart.C
+++ OutputGeometryPart.C
@@ -2056,7 +2056,8 @@ OutputGeometryPart::computeGroups(
         const int HAPI_PartInfo::*&maxMemberCount = maxMemberCounts[i];
         const MFn::Type fnType = fnTypes[i];
 
-        if(myGeoInfo.*groupCount == 0)
+        if(myGeoInfo.*groupCount == 0
+                || myPartInfo.*maxMemberCount == 0)
         {
             continue;
         }

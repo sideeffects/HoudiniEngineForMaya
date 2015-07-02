083cb87233e87ad6bff8bd66dbeae535cea11db2
Andrew Wong <awong@sidefx.com>
2015-07-02 14:48:23 -0400

    Fix bug where parameters under a folder are not being get/set correctly
    Those parameters are being incorrectly treated as ramp parameters.

--- Asset.C
+++ Asset.C
@@ -1091,7 +1091,9 @@ GetAttrOperation::leaf(const HAPI_ParmInfo &parmInfo)
                             parmInfo.intValuesIndex, parmInfo.size
                             );
 
-                    if(parentParmInfo && parentParmInfo->rampType != HAPI_RAMPTYPE_MAX)
+                    if(parentParmInfo
+                            && parentParmInfo->isChildOfMultiParm
+                            && parentParmInfo->rampType != HAPI_RAMPTYPE_MAX)
                     {
                         switch(enumIndex)
                         {
@@ -1496,7 +1498,9 @@ SetAttrOperation::leaf(const HAPI_ParmInfo &parmInfo)
                 }
                 else
                 {
-                    if(parentParmInfo && parentParmInfo->rampType != HAPI_RAMPTYPE_MAX)
+                    if(parentParmInfo
+                            && parentParmInfo->isChildOfMultiParm
+                            && parentParmInfo->rampType != HAPI_RAMPTYPE_MAX)
                     {
                         switch(enumIndex)
                         {

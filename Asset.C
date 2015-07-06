55e5c911bb4ec83581bb27be6e956e0ec2436a60
Andrew Wong <awong@sidefx.com>
2015-07-06 17:33:26 -0400

    Fix previous attempt to correctly detect ramp parameter

--- Asset.C
+++ Asset.C
@@ -1105,8 +1105,8 @@ GetAttrOperation::leaf(const HAPI_ParmInfo &parmInfo)
                             parmInfo.intValuesIndex, parmInfo.size
                             );
 
-                    if(parentParmInfo
-                            && parentParmInfo->isChildOfMultiParm
+                    if(parmInfo.isChildOfMultiParm
+                            && parentParmInfo
                             && parentParmInfo->rampType != HAPI_RAMPTYPE_MAX)
                     {
                         switch(enumIndex)
@@ -1522,8 +1522,8 @@ SetAttrOperation::leaf(const HAPI_ParmInfo &parmInfo)
                 }
                 else
                 {
-                    if(parentParmInfo
-                            && parentParmInfo->isChildOfMultiParm
+                    if(parmInfo.isChildOfMultiParm
+                            && parentParmInfo
                             && parentParmInfo->rampType != HAPI_RAMPTYPE_MAX)
                     {
                         switch(enumIndex)
--- util.C
+++ util.C
@@ -132,8 +132,8 @@ Util::getAttrNameFromParm(
         const HAPI_ParmInfo *parentParm
         )
 {
-    if(parentParm
-            && parentParm->isChildOfMultiParm
+    if(parm.isChildOfMultiParm
+            && parentParm
             && parentParm->rampType != HAPI_RAMPTYPE_MAX)
     {
         // Map the parameters of a Houdini ramp to the equivalent attributes of

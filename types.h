05735675aa674119f4dd3c63e5c975307970571c
Andrew Wong <awong@sidefx.com>
2015-10-11 19:17:58 -0400

    Add ArrayIterator

--- types.h
+++ types.h
@@ -35,4 +35,66 @@ struct ArrayTrait<RawArray<T> >
     { return array.size(); }
 };
 
+template<typename ArrayType>
+class ArrayIterator :
+    public std::iterator<std::input_iterator_tag, ArrayType>
+{
+    public:
+        typedef ArrayTrait<ArrayType> Trait;
+        typedef typename Trait::ElementType ElementType;
+
+        ArrayIterator(ArrayType& array, size_t index = 0) :
+            myArray(array), myIndex(index)
+        { }
+
+        ArrayIterator(const ArrayIterator<ArrayType> &o) :
+            myArray(o.myArray), myIndex(o.myIndex)
+        { }
+
+        ArrayIterator<ArrayType> &operator =(const ArrayIterator<ArrayType> &o)
+        {
+            myArray = o.myArray;
+            myIndex = o.myIndex;
+            return *this;
+        }
+
+        size_t operator -(const ArrayIterator<ArrayType> &o) const
+        { return myIndex - o.myIndex; }
+
+        ArrayIterator<ArrayType> &operator ++()
+        { ++myIndex; return *this; }
+
+        bool operator ==(const ArrayIterator<ArrayType> &o) const
+        { return myIndex == o.myIndex; }
+
+        bool operator !=(const ArrayIterator<ArrayType> &o) const
+        { return !(*this == o); }
+
+        const ElementType &operator *() const
+        { return Trait::getElement(myArray, myIndex); }
+
+        ElementType &operator *()
+        { return Trait::getElement(myArray, myIndex); }
+
+        const ElementType *operator ->() const
+        { return &Trait::getElement(myArray, myIndex); }
+
+        ElementType *operator ->()
+        { return &Trait::getElement(myArray, myIndex); }
+
+    private:
+        ArrayType &myArray;
+        size_t myIndex;
+};
+
+template<typename ArrayType>
+ArrayIterator<ArrayType>
+arrayBegin(ArrayType &array)
+{ return ArrayIterator<ArrayType>(array); }
+
+template<typename ArrayType>
+ArrayIterator<ArrayType>
+arrayEnd(ArrayType &array)
+{ return ArrayIterator<ArrayType>(array, ArrayTrait<ArrayType>::size(array)); }
+
 #endif

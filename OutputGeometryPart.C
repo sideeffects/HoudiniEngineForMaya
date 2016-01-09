400a969408cf438d660352c9da35f25870e59736
Damian Campeanu <damian@sidefx.com>
2016-01-09 18:55:53 -0500

    Added membership_array_all_equal parameter to HAPI_GetGroupMembership() in order to quickly tell if all items are in a group or all items are outside the group.

--- OutputGeometryPart.C
+++ OutputGeometryPart.C
@@ -2273,6 +2273,7 @@ OutputGeometryPart::computeGroups(
                     myAssetId, myObjectId, myGeoId, myPartId,
                     groupType,
                     groupName.asChar(),
+                    NULL,
                     &groupMembership[0],
                     0, groupMembership.size()
                     );

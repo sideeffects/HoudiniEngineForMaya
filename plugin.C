df8bcec35720eea8adc4d16c4baba58a0269ec69
Damian Campeanu <damian@sidefx.com>
2016-02-25 17:52:01 -0500

    Added setting of HAPI_CLIENT_NAME env variable on the Engine server side to "maya" for assets to know when they are running inside Maya.

--- plugin.C
+++ plugin.C
@@ -210,6 +210,13 @@ initializeHAPI(const OptionVars& optionVars)
         return false;
     }
 
+    // Set the client name.
+    HAPI_SetServerEnvString(
+        Util::theHAPISession.get(),
+        HAPI_ENV_CLIENT_NAME,
+        "maya"
+    );
+
     return true;
 }
 

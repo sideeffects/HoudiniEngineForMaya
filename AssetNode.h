e9119ecaba8c091e29ebf8d49ab93dd31a3a3450
Andrew Wong <awong@sidefx.com>
2017-09-01 18:51:15 -0400

    Fix warning about missing overloaded method
    The missing overloaded method is actually deprecated though.

--- AssetNode.h
+++ AssetNode.h
@@ -43,6 +43,7 @@ class AssetNode: public MPxTransform
         virtual bool setInternalValue(
                 const MPlug &plug,
                 const MDataHandle &dataHandle);
+        using MPxTransform::internalArrayCount;
         virtual int internalArrayCount(const MPlug &plug) const;
 #else
         virtual bool getInternalValueInContext(

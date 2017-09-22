ddc0babd126b5b8ae85442635c5afe10316160d2
Andrew Wong <awong@sidefx.com>
2017-09-22 19:05:34 -0400

    Reorder asset options

--- AssetNodeOptions.h
+++ AssetNodeOptions.h
@@ -4,13 +4,17 @@
 #include "NodeOptions.h"
 
 NODE_OPTIONS_BEGIN(AssetNodeOptions)
+
 NODE_OPTION(syncWhenInputConnects, bool, true)
 NODE_OPTION(autoSyncOutputs, bool, false)
+
+NODE_OPTION(useAssetObjectTransform, bool, false)
 NODE_OPTION(splitGeosByGroup, bool, false)
 NODE_OPTION(outputHiddenObjects, bool, false)
 NODE_OPTION(outputTemplatedGeometries, bool, false)
-NODE_OPTION(useAssetObjectTransform, bool, false)
+
 NODE_OPTION(useInstancerNode, bool, true)
+
 NODE_OPTIONS_END()
 
 extern AssetNodeOptions::Definition assetNodeOptionsDefinition;

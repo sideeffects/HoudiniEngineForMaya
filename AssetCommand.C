1f83b0e0ad572da37dc79ba56b3f2214de2d51c6
John Mather <johnmather@sidefx.com>
2020-04-13 17:05:12 -0400

    Parameter help strings are now displayed as tool-tips in the Maya Attribute Editor.

--- AssetCommand.C
+++ AssetCommand.C
@@ -34,6 +34,8 @@
 #define kSyncTemplatedGeosFlagLong "-syncTemplatedGeos"
 #define kAutoSyncIdFlag "-asi"
 #define kAutoSyncIdFlagLong "-autoSyncId"
+#define kParmHelpFlag "-ph"
+#define kParmHelpFlagLong "-parmHelp"
 
 const char *AssetCommand::commandName = "houdiniAsset";
 
@@ -136,6 +138,41 @@ public:
     }
 };
 
+class AssetSubCommandGetParmHelp : public SubCommandAsset
+{
+public:
+    AssetSubCommandGetParmHelp(const MObject &assetNodeObj,
+                               const MString &parmName)
+        : SubCommandAsset(assetNodeObj), myParmName(parmName)
+    {
+    }
+
+    virtual MStatus doIt()
+    {
+        HAPI_Result hapiResult;
+        HAPI_ParmInfo parmInfo;
+
+        MString helpString = "";
+
+        GET_COMMAND_ASSET_OR_RETURN_FAIL();
+
+        int nodeId = asset->getNodeInfo().id;
+
+        hapiResult = HAPI_GetParmInfoFromName(Util::theHAPISession.get(), nodeId,
+                                              myParmName.asChar(), &parmInfo);
+
+        if (parmInfo.helpSH != 0)
+            helpString = Util::HAPIString(parmInfo.helpSH);
+
+        MPxCommand::setResult(helpString);
+
+        return MStatus::kSuccess;
+    }
+
+protected:
+    MString myParmName;
+};
+
 void *
 AssetCommand::creator()
 {
@@ -201,6 +238,11 @@ AssetCommand::newSyntax()
     CHECK_MSTATUS(syntax.addFlag(
         kAutoSyncIdFlag, kAutoSyncIdFlagLong, MSyntax::kSelectionItem));
 
+    // -parmHelp will return the help string of the specified parm
+    CHECK_MSTATUS(syntax.addFlag(
+        kParmHelpFlag, kParmHelpFlagLong, MSyntax::kString,
+        MSyntax::kSelectionItem));
+
     return syntax;
 }
 
@@ -208,10 +250,11 @@ bool
 AssetCommand::getMObjectFromFlag(const MArgDatabase &argData,
                                  const char *flag,
                                  MObject &obj,
-                                 MStatus &status)
+                                 MStatus &status,
+                                 const int index)
 {
     MSelectionList selection;
-    status = argData.getFlagArgument(flag, 0, selection);
+    status = argData.getFlagArgument(flag, index, selection);
     if (!status)
     {
         MString error_message;
@@ -244,18 +287,20 @@ AssetCommand::parseArgs(const MArgList &args)
     }
 
     if (!(argData.isFlagSet(kListAssetsFlag) ^
-          argData.isFlagSet(kLoadAssetFlag) ^ argData.isFlagSet(kSyncFlag) ^
+          argData.isFlagSet(kLoadAssetFlag) ^
+          argData.isFlagSet(kSyncFlag) ^
           argData.isFlagSet(kResetSimulationFlag) ^
           argData.isFlagSet(kCookMessagesFlag) ^
           argData.isFlagSet(kReloadAssetFlag) ^
           argData.isFlagSet(kReloadNoSyncFlag) ^
-          argData.isFlagSet(kAutoSyncIdFlag)))
+          argData.isFlagSet(kAutoSyncIdFlag) ^
+          argData.isFlagSet(kParmHelpFlag)))
     {
         displayError(
             "Exactly one of these flags must be specified:\n" kLoadAssetFlagLong
             "\n" kSyncFlagLong "\n" kResetSimulationFlagLong
             "\n" kCookMessagesFlagLong "\n" kReloadNoSyncFlagLong
-            "\n" kReloadAssetFlagLong "\n");
+            "\n" kReloadAssetFlagLong "\n" kParmHelpFlag "\n");
         return MStatus::kInvalidParameter;
     }
 
@@ -391,6 +436,26 @@ AssetCommand::parseArgs(const MArgList &args)
             return status;
 
         mySubCommand = new AssetSubCommandCookMessages(assetNodeObj);
+    }
+
+    if (argData.isFlagSet(kParmHelpFlag))
+    {
+        MString parmName;
+        {
+            status = argData.getFlagArgument(kParmHelpFlag, 0, parmName);
+            if (!status)
+            {
+                displayError("Invalid argument for \"" kParmHelpFlagLong "\".");
+                return status;
+            }
+        }
+
+        MObject assetNodeObj;
+        if (!getMObjectFromFlag(
+                argData, kParmHelpFlag, assetNodeObj, status, 1))
+            return status;
+
+        mySubCommand = new AssetSubCommandGetParmHelp(assetNodeObj, parmName);
     }
 
     return MStatus::kSuccess;

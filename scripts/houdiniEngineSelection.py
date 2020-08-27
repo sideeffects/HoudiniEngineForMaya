db8c437d7fabe31592106cc11459c46760fe7ef3
John Mather <johnmather@sidefx.com>
2020-08-27 16:47:31 -0400

    Reverting r367875 due to prop change on directory

--- scripts/houdiniEngineSelection.py
+++ scripts/houdiniEngineSelection.py
@@ -4,59 +4,35 @@ def get_selected_components(component_type):
     components = []
     sel_mask   = -1
     type_name  = ''
-    expand     = False
 
-    lwr_component_type = component_type.lower()
-
-    if lwr_component_type == 'vertex':
-        sel_mask  = 31
+    if component_type.lower() == 'vertex':
+        sel_mask = 31
         type_name = 'vertices'
-    elif lwr_component_type == 'edge':
-        sel_mask  = 32
+    elif component_type.lower() == 'edge':
+        sel_mask = 32
         type_name = 'edges'
-        expand    = True
-    elif lwr_component_type == 'face':
-        sel_mask  = 34
+    elif component_type.lower() == 'face':
+        sel_mask = 34
         type_name = 'faces'
-    elif lwr_component_type == 'uv':
-        sel_mask  = 35
+    elif component_type.lower() == 'uv':
+        sel_mask = 35
         type_name = 'UVs'
     else:
         cmds.error('Unknown component type. Must be "vertex", "edge", "face" or "uv".')
 
-    def extract_component(component):
+    components_raw = cmds.filterExpand(cmds.ls(sl=True), sm=sel_mask, ex=False)
+
+    if not components_raw:
+        return None
+
+    for component in components_raw:
         lidx = component.find('[') + 1
         ridx = component.rfind(']')
 
         if lidx <= 0 or ridx <= 0 or ridx <= lidx:
             cmds.warning('Could not determine %s index of "%s"' % (type_name, component))
 
-        return component[lidx:ridx]
+        components.append(component[lidx:ridx].replace(':', '-'))
 
-    components_raw = cmds.filterExpand(cmds.ls(sl=True), sm=sel_mask, ex=expand)
+    return ', '.join(components)
 
-    if not components_raw:
-        return None
-
-    # Edges are handled differently in Houdini. Instead of an edge index, it is
-    # specified as two points with a "p" prefix. Eg "p1-3" represents an edge
-    # between point 1 and point 3.
-    if lwr_component_type == 'edge':
-        vertex_components = []
-
-        for component in components_raw:
-            raw_vtx_components = cmds.polyListComponentConversion(component, fe=True, tv=True)
-            
-            if len(raw_vtx_components) == 2:
-                vertex_components.append('p' + '-'.join([extract_component(raw_vtx_component) for raw_vtx_component in raw_vtx_components]))
-            elif len(raw_vtx_components) == 1:
-                vertex_components.append('p' + extract_component(raw_vtx_components[0]).replace(':', '-'))
-            else:
-                cmds.error('Unable to convert edge with %d vertices to a single range of vertices.' % len(raw_vtx_components))
-            
-        return ' '.join(vertex_components)
-
-    for component in components_raw:
-        components.append(extract_component(component).replace(':', '-'))
-
-    return ' '.join(components)

6ddda3ffe27f1e5aa3fa989fc3b8e6801efb33ba
John Mather <johnmather@sidefx.com>
2026-09-09 16:17:51 -0400

    Backport dev r553586:
    Add support for COPs and Principled Shaders

--- OutputMaterial.C
+++ OutputMaterial.C
@@ -14,6 +14,97 @@ OutputMaterial::OutputMaterial(HAPI_NodeId assetId)
 {
 }
 
+static int findMaterialParmIndex(std::vector<HAPI_ParmInfo> &parms,
+    const std::vector<MString> names, const std::vector<int> instanceNums)
+{
+    int parmIndex = -1;
+    const int size = names.size();
+    for (int i = 0; i < size; i++)
+    {
+        const MString &name = names[i];
+        const int instanceNum = instanceNums[i];
+        parmIndex = Util::findParm(parms, name, instanceNum);
+        if (parmIndex >= 0)
+            return parmIndex;
+    }
+    return parmIndex;
+}
+
+HAPI_Result OutputMaterial::handleDiffuseTexture(const int texturePathSHParmIndex, MDataHandle &texturePathHandle)
+{
+    HAPI_Result hapiResult;
+
+    HAPI_ParmInfo texturePathParm;
+    HoudiniApi::GetParameters(Util::theHAPISession.get(), myNodeId,
+                        &texturePathParm, texturePathSHParmIndex, 1);
+
+    int texturePathSH;
+    HoudiniApi::GetParmStringValues(Util::theHAPISession.get(), myNodeId, true,
+                                &texturePathSH,
+                                texturePathParm.stringValuesIndex, 1);
+
+    bool hasTextureSource =
+        ((std::string)Util::HAPIString(texturePathSH)).size() > 0;
+    bool canRenderTexture = false;
+    if (hasTextureSource && myBakeTexture)
+    {
+        // this could fail if texture parameter is empty
+        hapiResult = HoudiniApi::RenderTextureToImage(
+            Util::theHAPISession.get(), myNodeId,
+            texturePathSHParmIndex);
+
+        canRenderTexture = hapiResult == HAPI_RESULT_SUCCESS;
+    }
+
+    int destinationFilePathSH = 0;
+    MString destinationFolderPath;
+    MGlobal::executeCommand(
+        "workspace -expandName "
+        "`workspace -q -fileRuleEntry sourceImages`;",
+        destinationFolderPath);
+
+    if (canRenderTexture && myBakeTexture)
+    {
+        // this could fail if the image planes don't exist
+        hapiResult = HoudiniApi::ExtractImageToFile(
+            Util::theHAPISession.get(), myNodeId, HAPI_PNG_FORMAT_NAME,
+            "C A", destinationFolderPath.asChar(), NULL,
+            &destinationFilePathSH);
+        if (HAPI_FAIL(hapiResult))
+        {
+            DISPLAY_ERROR("Could not extract image to directory:\n"
+                            "    ^1s",
+                            destinationFolderPath);
+            DISPLAY_ERROR_HAPI_STATUS_CALL();
+        }
+    }
+
+    if (hasTextureSource && !myBakeTexture)
+    {
+        // if baking is off but the expected texture file exists
+        // keep using it
+        hapiResult = HoudiniApi::GetImageFilePath(
+            Util::theHAPISession.get(), myNodeId, HAPI_PNG_FORMAT_NAME,
+            "C A", destinationFolderPath.asChar(), NULL,
+            texturePathSHParmIndex, &destinationFilePathSH);
+
+        MString texturePath = Util::HAPIString(destinationFilePathSH);
+        MString cmd         = "filetest -e \"" + texturePath + "\"";
+        int fileExists;
+        MGlobal::executeCommand(cmd, fileExists);
+        if (!fileExists)
+            destinationFilePathSH = 0;
+    }
+
+    if (destinationFilePathSH > 0)
+    {
+        MString texturePath = Util::HAPIString(destinationFilePathSH);
+        texturePathHandle.set(texturePath);
+    }
+
+    return hapiResult;
+}
+
 MStatus
 OutputMaterial::compute(const MTime &time,
                         const MPlug &materialPlug,
@@ -21,8 +112,6 @@ OutputMaterial::compute(const MTime &time,
                         MDataHandle &materialHandle,
                         bool bakeTexture)
 {
-    HAPI_Result hapiResult;
-
     data.setClean(materialPlug);
 
     update(materialHandle);
@@ -62,11 +151,11 @@ OutputMaterial::compute(const MTime &time,
         HoudiniApi::GetParameters(Util::theHAPISession.get(), myNodeId, &parms[0], 0,
                            myNodeInfo.parmCount);
 
-        int ambientParmIndex       = Util::findParm(parms, "ogl_amb");
-        int diffuseParmIndex       = Util::findParm(parms, "ogl_diff");
-        int alphaParmIndex         = Util::findParm(parms, "ogl_alpha");
-        int specularParmIndex      = Util::findParm(parms, "ogl_spec");
-        int texturePathSHParmIndex = Util::findParm(parms, "ogl_tex#", 1);
+        int ambientParmIndex       = findMaterialParmIndex(parms, {"ogl_amb", "emitcolor"}, {-1, -1});
+        int diffuseParmIndex       = findMaterialParmIndex(parms, {"ogl_diff", "basecolor"}, {-1, -1});
+        int alphaParmIndex         = findMaterialParmIndex(parms, {"ogl_alpha", "transparency"}, {-1, -1});
+        int specularParmIndex      = findMaterialParmIndex(parms, {"ogl_spec"}, {-1});
+        int texturePathSHParmIndex = findMaterialParmIndex(parms, {"ogl_tex#", "basecolor_texture"}, {1, -1});
         float valueHolder[4];
 
         nameHandle.setString(Util::HAPIString(myNodeInfo.nameSH));
@@ -76,6 +165,19 @@ OutputMaterial::compute(const MTime &time,
             HoudiniApi::GetParmFloatValues(
                 Util::theHAPISession.get(), myNodeId, valueHolder,
                 parms[ambientParmIndex].floatValuesIndex, 3);
+            
+            const int emitIntensityParmIndex = findMaterialParmIndex(parms, {"emitint"}, {-1});
+            if (emitIntensityParmIndex >= 0)
+            {
+                float emitIntensityValueHolder[4];
+                HoudiniApi::GetParmFloatValues(Util::theHAPISession.get(), myNodeId,
+                    emitIntensityValueHolder,
+                    parms[emitIntensityParmIndex].floatValuesIndex, 1);
+
+                for (int i = 0; i < 3; i++)
+                    valueHolder[i] *= emitIntensityValueHolder[0];
+            }
+
             ambientHandle.set3Float(
                 valueHolder[0], valueHolder[1], valueHolder[2]);
         }
@@ -91,11 +193,22 @@ OutputMaterial::compute(const MTime &time,
 
         if (diffuseParmIndex >= 0)
         {
-            HoudiniApi::GetParmFloatValues(
-                Util::theHAPISession.get(), myNodeId, valueHolder,
-                parms[diffuseParmIndex].floatValuesIndex, 3);
-            diffuseHandle.set3Float(
-                valueHolder[0], valueHolder[1], valueHolder[2]);
+            HAPI_ParmInfo diffuseParmInfo;
+            HoudiniApi::GetParameters(Util::theHAPISession.get(), myNodeId,
+                &diffuseParmInfo, diffuseParmIndex, 1);
+
+            // "basecolor" is a color in the Principled Shader, but a texture in
+            // the COP Viewer Material.
+            if (diffuseParmInfo.type == HAPI_PARMTYPE_COLOR)
+            {
+                HoudiniApi::GetParmFloatValues(
+                    Util::theHAPISession.get(), myNodeId, valueHolder,
+                    parms[diffuseParmIndex].floatValuesIndex, 3);
+                diffuseHandle.set3Float(
+                    valueHolder[0], valueHolder[1], valueHolder[2]);
+            }
+            else
+                handleDiffuseTexture(diffuseParmIndex, texturePathHandle);
         }
 
         if (alphaParmIndex >= 0)
@@ -103,80 +216,15 @@ OutputMaterial::compute(const MTime &time,
             HoudiniApi::GetParmFloatValues(Util::theHAPISession.get(), myNodeId,
                                     valueHolder,
                                     parms[alphaParmIndex].floatValuesIndex, 1);
-            float alpha = 1 - valueHolder[0];
+
+            float alpha = valueHolder[0];
+            if (Util::findParm(parms, "ogl_alpha", -1) >= 0)
+                alpha = 1.0f - valueHolder[0];
             alphaHandle.set3Float(alpha, alpha, alpha);
         }
 
         if (texturePathSHParmIndex >= 0)
-        {
-            HAPI_ParmInfo texturePathParm;
-            HoudiniApi::GetParameters(Util::theHAPISession.get(), myNodeId,
-                               &texturePathParm, texturePathSHParmIndex, 1);
-
-            int texturePathSH;
-            HoudiniApi::GetParmStringValues(Util::theHAPISession.get(), myNodeId, true,
-                                     &texturePathSH,
-                                     texturePathParm.stringValuesIndex, 1);
-
-            bool hasTextureSource =
-                ((std::string)Util::HAPIString(texturePathSH)).size() > 0;
-            bool canRenderTexture = false;
-            if (hasTextureSource && bakeTexture)
-            {
-                // this could fail if texture parameter is empty
-                hapiResult = HoudiniApi::RenderTextureToImage(
-                    Util::theHAPISession.get(), myNodeId,
-                    texturePathSHParmIndex);
-
-                canRenderTexture = hapiResult == HAPI_RESULT_SUCCESS;
-            }
-
-            int destinationFilePathSH = 0;
-            MString destinationFolderPath;
-            MGlobal::executeCommand(
-                "workspace -expandName "
-                "`workspace -q -fileRuleEntry sourceImages`;",
-                destinationFolderPath);
-
-            if (canRenderTexture && bakeTexture)
-            {
-                // this could fail if the image planes don't exist
-                hapiResult = HoudiniApi::ExtractImageToFile(
-                    Util::theHAPISession.get(), myNodeId, HAPI_PNG_FORMAT_NAME,
-                    "C A", destinationFolderPath.asChar(), NULL,
-                    &destinationFilePathSH);
-                if (HAPI_FAIL(hapiResult))
-                {
-                    DISPLAY_ERROR("Could not extract image to directory:\n"
-                                  "    ^1s",
-                                  destinationFolderPath);
-                    DISPLAY_ERROR_HAPI_STATUS_CALL();
-                }
-            }
-
-            if (hasTextureSource && !bakeTexture)
-            {
-                // if baking is off but the expected texture file exists
-                // keep using it
-                hapiResult = HoudiniApi::GetImageFilePath(
-                    Util::theHAPISession.get(), myNodeId, HAPI_PNG_FORMAT_NAME,
-                    "C A", destinationFolderPath.asChar(), NULL,
-                    texturePathSHParmIndex, &destinationFilePathSH);
-
-                MString texturePath = Util::HAPIString(destinationFilePathSH);
-                MString cmd         = "filetest -e \"" + texturePath + "\"";
-                int fileExists;
-                MGlobal::executeCommand(cmd, fileExists);
-                if (!fileExists)
-                    destinationFilePathSH = 0;
-            }
-
-            if (destinationFilePathSH > 0)
-            {
-                MString texturePath = Util::HAPIString(destinationFilePathSH);
-                texturePathHandle.set(texturePath);
-            }
-        }
+            handleDiffuseTexture(texturePathSHParmIndex, texturePathHandle);
 
         myMaterialLastCookCount = myNodeInfo.totalCookCount;
     }
